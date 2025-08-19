71631b25ac85a749e9feeaed2177b5eec54ef994
hostadam <hostadam.minecraft@gmail.com>
2025-08-19 16:01:03 +0200

    Revert "a"
    This reverts commit 21dd0521387c2bd5a3148b565430bb60a8277029.

--- src/main/java/io/github/hostadam/config/locale/performance/CachedComponent.java
+++ src/main/java/io/github/hostadam/config/locale/performance/CachedComponent.java
@@ -1,19 +1,5 @@
 package io.github.hostadam.config.locale.performance;
 
-import io.github.hostadam.config.locale.PlaceholderProvider;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.JoinConfiguration;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public record CachedComponent(List<ComponentVariant> variants) {
-
-    public Component resolve(PlaceholderProvider provider) {
-        List<Component> resolved = new ArrayList<>(variants.size());
-        for (ComponentVariant variant : variants) {
-            resolved.add(variant.resolve(provider));
-        }
-        return Component.join(JoinConfiguration.newlines(), resolved);
-    }
-}
+public record CachedComponent(Component component, boolean containsPlaceholders, boolean needsExtraRoundtrip) { }
